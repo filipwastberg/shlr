adaca6a616391277f7b7fea64fc15f1edbdb1693
filipwastberg <filip.wastberg@gmail.com>
2019-02-10 11:33:59 +0100

    fixed dependecies

---
+++ R/shlr.R
@@ -0,0 +1,22 @@
+#' \code{shlr} package
+#'
+#' A Quick and Easy Wrapper for data from SHL's Open API into R
+#'
+#' See the README on
+#' \href{https://github.com/filipwastberg/shlr#readme}{GitHub}
+#'
+#' @docType package
+#' @name shlr
+#' @import purrr
+#' @import dplyr
+#' @import httr
+#' @import stringr
+#' @importFrom chron chron
+NULL
+##
+##globalVars <- c(
+##  ".")
+##
+#### quiets concerns of R CMD check re: the .'s that appear in pipelines
+##if(getRversion() >= "2.15.1")  utils::globalVariables(globalVars)
+##
